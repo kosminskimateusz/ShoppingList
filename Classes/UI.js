703606545a334e992b5ff2f53900f751bdeecc5a
Mateusz Kosminski <kosminskimateusz@gmail.com>
2022-02-18 20:47:40 +0100

    Adding product validation. Show product after adding new one.

--- Classes/UI.js
+++ Classes/UI.js
@@ -1,29 +1,66 @@
 import { Product } from "./Product.js";
 
-export class UI{
+export class UI {
   productList = [
     new Product("Olej", 5),
     new Product("Kokos", 12)
   ];
+
   constructor() {
-    
+    localStorage.setItem('productList', JSON.stringify(this.productList));
+    const temp = localStorage.getItem('productList');
+    console.log(JSON.parse(temp));
   }
-  
+
 
   ShowProducts() {
     // Represent data in table on DOM Loaded
+    const table = document.querySelector("#product-list");
+    table.innerHTML = '';
+    this.productList.forEach(element => {
+      const product = document.createElement('tr');
+      product.innerHTML = `
+      <td>${element.name}</td>
+      <td>${element.price}</td>
+      <td><a href="#">DELETE</a></td>
+      `;
+      table.appendChild(product);
+    });
     console.log(this.productList);
   }
 
-  GetInputProduct() {
-    const name = document.querySelector("#name").value;
-    const price = parseInt(document.querySelector("#price").value);
-    
-    return {name, price};
-  }
+  // ADD PRODUCT
   AddProduct() {
-    const productData = this.GetInputProduct();
-    const product = new Product(productData.name, productData.price);
-    this.productList.push(product);
+    const productData = this.#GetInputProduct();
+    if (this.#Validation(productData)) {
+      this.#ClearInput();
+      const product = new Product(productData.name, parseInt(productData.price));
+      this.productList.push(product);
+      this.ShowProducts();
+    } else {
+      alert("Incorrect input");
+    }
   }
+
+
+  #GetInputProduct() {
+    const name = document.querySelector("#name").value;
+    const price = document.querySelector("#price").value;
+    return { name, price };
+  }
+
+  // CLEAR INPUT
+  #ClearInput() {
+    document.querySelector("#name").value = null;
+    document.querySelector("#price").value = null;
+  }
+
+  // INPUT VALIDATION
+  #Validation(data) {
+    if (data.name === '' || data.price === '') {
+      return false;
+    }
+    return true;
+  }
+
 }
--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-// import { Product } from "./Classes/Product.js";
 import { UI } from "./Classes/UI.js";
 
-// const product = new Product("Makaron", 4);
 const ui = new UI();
-ui.ShowProducts();
-ui.AddProduct();
+// ui.ShowProducts();
+// ui.AddProduct();
 
 // ui.ShowProducts();
 
+window.addEventListener("DOMContentLoaded", () => {
+  ui.ShowProducts();
+});
+
+// document.querySelector("#shop-form").addEventListener("submit", () => {
+//   ui.AddProduct();
+// })
+
+document.querySelector("#submit").addEventListener("click", () => {
+  ui.AddProduct();
+})
